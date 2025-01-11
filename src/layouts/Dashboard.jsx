909aec951766438217c566d67a4d260099ac5243
ercancelikbusiness <ercancelikbusiness@gmail.com>
2025-01-11 18:02:32 +0300

    Dashboard update

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Navi from './Navi'
 import Categories from './Categories'
 import ProductList from '../pages/ProductList'
-import { GridRow, GridColumn, Grid, } from 'semantic-ui-react'
-import { Route } from 'react-router-dom'
-
+import { Grid } from 'semantic-ui-react'
+import { Routes, Route } from 'react-router-dom'
+import ProductDetail from '../pages/ProductDetail'
 
 export default function Dashboard() {
   return (
@@ -15,14 +15,20 @@ export default function Dashboard() {
             <Categories />
           </Grid.Column>
           <Grid.Column width={12}>
-           <Route>
-             
-           </Route>
+            <Routes> //eskiden bu route imiş güncelleme ile routes olmuş
+             // Route path="/" // sadece / varsa tarayıcı url alanında devamında  ne olursa olsun  mesela productlisti açsın demek
+             // örn: http://localhost:3000/products/asdwadsadawdasd
+
+             <Route exact path="/" Component={ProductList}/>
+             <Route exact path="/products" Component={ProductList}/>
+             <Route path="/products/:id" Component={ProductDetail}/>
+             //burdaki :id ise products/ dan sonra 12 232 213123 gibi numara verirsen url ye onu acıcak demek
+
+            </Routes>
           </Grid.Column>
         </Grid.Row>
       </Grid>
-
-
-    </div>
+    </div> 
   )
 }
+
